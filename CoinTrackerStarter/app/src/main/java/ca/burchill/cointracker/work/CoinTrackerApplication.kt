f7b05bc3be3f695d34db554d25e5d5dc2a09f5fe
Marcio de Freitas Nascimento <59565829+marcionascimentonb@users.noreply.github.com>
2021-04-10 17:58:39 -0300

    progress

--- CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/work/CoinTrackerApplication.kt
+++ CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/work/CoinTrackerApplication.kt
@@ -1,7 +1,51 @@
 package ca.burchill.cointracker.work
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class CoinTrackerApplication : Application() {
-    // TODO - implement delayedInit() that ensures workmanager is refreshing the database regularly
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+
 }
